4ea0e3a81765c4ccc6af5c8c8198ca62a246523b
SurajSaiGudala <surajsaiis20@gmail.com>
2025-03-28 09:15:23 +0530

    A New-method inside logical-building/Pattern.java was created and executed successfully 😊

--- logical-building/Pattern.java
+++ logical-building/Pattern.java
@@ -18,9 +18,18 @@ public class Pattern
                 System.out.println();
             }
         }
-    
+        private static void pattern3(int N) {
+            System.out.println("Printing a Right-Triangle-Number-Pattern of size-"+N+"....");
+            for(int i=1;i<=N;i++) {
+                for(int j=1;j<=i;j++) {
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+        }
     public static void main(String[] args) {
         new Pattern().pattern1(5);
         new Pattern().pattern2(5);
+        new Pattern().pattern3(5);
     }
 }
