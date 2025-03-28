b44ed1ed8368c002f28d27ad1988e789dd2531a8
SurajSaiGudala <surajsaiis20@gmail.com>
2025-03-28 08:40:04 +0530

    A new method has been added inside locaical-buiding/Patten.java 😊

--- logical-building/Pattern.java
+++ logical-building/Pattern.java
@@ -9,7 +9,18 @@ public class Pattern
         System.out.println();    
         }
     }
+        private static void pattern2(int N) {
+            System.out.println("Printing a Right-Triangle-Pattern of size-"+N+"....");
+            for(int i=1;i<=N;i++) {
+                for(int j=0;j<i;j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    
     public static void main(String[] args) {
         new Pattern().pattern1(5);
+        new Pattern().pattern2(5);
     }
 }
