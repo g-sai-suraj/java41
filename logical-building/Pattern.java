c3ad31695db21708e203293286884f87981dcdca
SurajSaiGudala <surajsaiis20@gmail.com>
2025-04-05 15:11:31 +0530

    A new-method (pattern18(int N)) was created inside 'Pattern.java' and executed successfully 😊

--- logical-building/Pattern.java
+++ logical-building/Pattern.java
@@ -245,6 +245,33 @@ public class Pattern
                 System.out.println();
             }
         }
+        private static void pattern18(int N) {
+            int iniS = 0;
+            System.out.println("Priting the pattern of the Symmetric-Pattern ...");
+            for(int i=0;i<N;i++) {
+                for(int j=1;j<=N-i;j++) {
+                    System.out.print("*");
+                } for(int j=0;j<iniS;j++) {
+                    System.out.print(" ");
+                } for(int j=1;j<=N-i;j++) {
+                    System.out.print("*");
+                }
+                iniS += 2;
+                System.out.println();
+            }
+            iniS = 2*N-2;
+            for(int i=1;i<=N;i++) {
+                for(int j=1;j<=i;j++) {
+                    System.out.print("*");
+                } for(int j=0;j<iniS;j++) {
+                    System.out.print(" ");
+                } for(int j=1;j<=i;j++) {
+                    System.out.print("*");
+                }
+                iniS -= 2;
+                System.out.println();
+            }
+        }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
        
@@ -265,6 +292,7 @@ public class Pattern
         pattern15(5);
         pattern16(5);
         pattern17(5);
+        pattern18(5);
         sc.close();
     }
 }
